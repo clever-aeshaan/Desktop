2e06671a9ce7441a6f923fcb069d83feefa19181
clever-aeshaan <aeshaanp@gmail.com>
2026-08-23 21:14:47 -0400

    class 1 and class2

---
+++ L3A2.py
@@ -0,0 +1,29 @@
+name="penguin"
+
+age=15
+
+is_student=True
+
+weight=38.5
+
+print(name)
+print(type(name))
+
+print(age)
+print(type(age))
+
+print(is_student)
+print(type(is_student))
+
+print(weight)
+print(type(weight))
+
+print("after typecasting")
+x = float(age)
+print(x)
+print(type(x))
+
+print("after typecasting")
+e = int(weight)
+print(e)
+print(type(e))
