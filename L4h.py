9325025a4e2299ac1024784477c02e7ba9613ead
clever-aeshaan <aeshaanp@gmail.com>
2026-08-27 20:13:32 -0400

    class 1 and class2

---
+++ L4h.py
@@ -0,0 +1,5 @@
+number = 4
+square = number ** 2
+print("the number of nuts mark had is 4. He is trying square the mount of nuts he had")
+
+print("After he did the math he ended up with", + square)
