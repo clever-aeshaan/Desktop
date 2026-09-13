5633fdbe328fcd4708a3e7a10c845afcef9355e3
clever-aeshaan <aeshaanp@gmail.com>
2026-09-13 17:07:36 -0400

    class 7

---
+++ L7h.py
@@ -0,0 +1,12 @@
+age_input = input("Enter your age: ")
+
+try:
+    age = int(age_input)
+    
+    if age >= 10 and age <= 20:
+        print("The age is between 10 and 20.")
+    else:
+        print("The age is NOT between 10 and 20.")
+
+except ValueError:
+    print("Invalid input! Please enter a valid whole number for age.")
