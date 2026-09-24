cc320e0feca1ef2af3bc0a272c0f14cdd3cbda3a
clever-aeshaan <aeshaanp@gmail.com>
2026-09-23 20:50:28 -0400

    class 11

---
+++ codingal/L11A1.py
@@ -0,0 +1,8 @@
+print("right angles with stars(*)")
+num = int(input("enter the number of rows"))
+
+for i in range(0, num):
+    for j in range(0, i+1):
+
+        print("* ", end = "")
+    print() 
